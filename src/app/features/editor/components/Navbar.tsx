013ab0dab21ca75cf1f015a2d54ee7869ab9cafc
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-05 00:43:24 +0530

    Enhance Navbar with export functionality and additional file type options; update icons and layout

--- src/app/features/editor/components/Navbar.tsx
+++ src/app/features/editor/components/Navbar.tsx
@@ -8,16 +8,29 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { ChevronDown, MousePointerClick, Redo2, Undo2 } from 'lucide-react';
+import {
+  ChevronDown,
+  Download,
+  MousePointerClick,
+  Redo2,
+  Undo2,
+} from 'lucide-react';
 import { CiFileOn } from 'react-icons/ci';
 import { Separator } from '@/components/ui/separator';
 import { Hint } from '@/components/crevo/Hint';
+import {
+  BsCloudCheck,
+  BsFiletypeJpg,
+  BsFiletypeJson,
+  BsFiletypePng,
+  BsFiletypeSvg,
+} from 'react-icons/bs';
 
 const Navbar = () => {
   return (
     <nav className="w-full flex items-center p-4 h-[64px] gap-x-8 border-b lg:pl-[34px]">
       <Logo />
-      <div className="flex full-w items-center gax-x-1 h-full">
+      <div className="flex w-full items-center gap-x-1 h-full">
         <DropdownMenu modal={false}>
           <DropdownMenuTrigger asChild>
             <Button size="sm" variant="ghost">
@@ -30,7 +43,7 @@ const Navbar = () => {
               onClick={() => {}}
               className="flex flex-row items-center gap-x-2"
             >
-              <CiFileOn className="size-8" />
+              <CiFileOn style={{ width: '1.5rem', height: '1.5rem' }} />
               <div>
                 <p>Open</p>
                 <p className="text-xs text-muted-foreground">
@@ -56,6 +69,75 @@ const Navbar = () => {
             <Redo2 className="size-4" />
           </Button>
         </Hint>
+        <Separator orientation="vertical" className="mx-2" />
+        <div className="flex items-center gap-x-2">
+          <BsCloudCheck className="size-[20px] text-muted-foreground" />
+          <p className="text-xs text-muted-foreground">Saved</p>
+        </div>
+        <div
+          className="ml-auto flex items-center gap-x-4"
+          style={{ marginLeft: 'auto' }}
+        >
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <Button size={'sm'} variant="ghost">
+                Export
+                <Download className="size-4 ml-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="min-w-60">
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <BsFiletypeJson style={{ width: '1.5rem', height: '1.5rem' }} />
+                <div>
+                  <p>JSON</p>
+                  <p className="text-xs text-muted-foreground">
+                    Save for later editing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <BsFiletypePng style={{ width: '1.5rem', height: '1.5rem' }} />
+                <div>
+                  <p>PNG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Best for sharing on the web
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <BsFiletypeJpg style={{ width: '1.5rem', height: '1.5rem' }} />
+                <div>
+                  <p>JPG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Best for printing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <BsFiletypeSvg style={{ width: '1.5rem', height: '1.5rem' }} />
+                <div>
+                  <p>SVG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Best for editing in vector software
+                  </p>
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          {/* ToDO Add User Btn */}
+        </div>
       </div>
     </nav>
   );
