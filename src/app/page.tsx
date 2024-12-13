1f9c4f9e5d8270590673e0690bd49316ba656e7f
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-13 12:20:17 +0530

    Add authentication middleware and database schema; implement NextAuth with Drizzle adapter and PostgreSQL support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/editor/test">
-        <Button>Click me</Button>
-      </Link>
-    </div>
-  );
+import { auth } from '@/auth';
+export default async function Home() {
+  const session = await auth();
+  return <div></div>;
 }
