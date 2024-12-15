3967b37b1217fefc169382d6723e3a029fa63dad
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-15 17:09:29 +0530

    Add project retrieval and error handling; implement useGetProject hook and update editor page

--- src/app/api/[[...route]]/projects.ts
+++ src/app/api/[[...route]]/projects.ts
@@ -2,47 +2,74 @@ import { db } from '@/db/drizzle';
 import { projectsInsertSchema, projects } from '@/db/schema';
 import { verifyAuth } from '@hono/auth-js';
 import { zValidator } from '@hono/zod-validator';
+import { eq, and } from 'drizzle-orm';
 import { Hono } from 'hono';
+import { z } from 'zod';
 
-const app = new Hono().post(
-  '/',
-  verifyAuth(),
-  zValidator(
-    'json',
-    projectsInsertSchema.pick({
-      name: true,
-      json: true,
-      height: true,
-      width: true,
-    })
-  ),
-  async (c) => {
-    const auth = c.get('authUser');
-    const { name, json, height, width } = c.req.valid('json');
+const app = new Hono()
+  .get(
+    '/:id',
+    verifyAuth(),
+    zValidator('param', z.object({ id: z.string() })),
+    async (c) => {
+      const auth = c.get('authUser');
+      const { id } = c.req.valid('param');
 
-    if (!auth.token?.id) {
-      return c.json({ error: 'Unauthorized' }, 401);
+      if (!auth.token?.id) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      const data = await db
+        .select()
+        .from(projects)
+        .where(and(eq(projects.id, id), eq(projects.userId, auth.token.id)));
+
+      if (data?.length === 0) {
+        return c.json({ error: 'Project not found' }, 404);
+      }
+
+      return c.json({ data: data[0] });
     }
-
-    const data = await db
-      .insert(projects)
-      .values({
-        name,
-        json,
-        width,
-        height,
-        userId: auth.token.id,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+  )
+  .post(
+    '/',
+    verifyAuth(),
+    zValidator(
+      'json',
+      projectsInsertSchema.pick({
+        name: true,
+        json: true,
+        height: true,
+        width: true,
       })
-      .returning();
+    ),
+    async (c) => {
+      const auth = c.get('authUser');
+      const { name, json, height, width } = c.req.valid('json');
 
-    if (!data[0]) {
-      return c.json({ error: 'Something went wrong' }, 400);
+      if (!auth.token?.id) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      const data = await db
+        .insert(projects)
+        .values({
+          name,
+          json,
+          width,
+          height,
+          userId: auth.token.id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+        .returning();
+
+      if (!data[0]) {
+        return c.json({ error: 'Something went wrong' }, 400);
+      }
+
+      return c.json({ data: data[0] });
     }
-
-    return c.json({ data: data[0] });
-  }
-);
+  );
 
 export default app;
--- src/app/editor/[project]/page.tsx
+++ src/app/editor/[project]/page.tsx
@@ -1,8 +1,36 @@
-import { protectServer } from '@/app/features/auth/utils';
+'use client';
+import { useGetProject } from '@/app/features/projects/api/use-get-projects';
 import Editor from '@/app/features/editor/components/editor';
+import { Loader, TriangleAlert } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
-const EditorPage = async () => {
-  await protectServer();
+interface EditorPageProps {
+  params: {
+    project: string;
+  };
+}
+
+const EditorPage = ({ params }: EditorPageProps) => {
+  const { data, isLoading, isError } = useGetProject(params.project);
+  if (isLoading || !data) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center">
+        <TriangleAlert className="size-6 animate-spin text-muted-foreground" />
+        <p className="text-sm text-muted-foreground">Failed to load project</p>
+        <Button asChild variant={'secondary'}>
+          <Link href={'/'}>Go Back</Link>
+        </Button>
+      </div>
+    );
+  }
   return <Editor />;
 };
 
