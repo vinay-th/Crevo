563826961f11dd0596c75ef3f06fa3dddf07f5ba
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-16 20:44:00 +0530

    Add subscription table and schema; implement foreign key constraint for userId

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -120,3 +120,21 @@ export const projectsRelation = relations(projects, ({ one }) => ({
 }));
 
 export const projectsInsertSchema = createInsertSchema(projects);
+
+export const subscriptions = pgTable('subscription', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  userId: text('userId')
+    .notNull()
+    .references(() => users.id, {
+      onDelete: 'cascade',
+    }),
+  subscriptionId: text('subscriptionId').notNull(),
+  customerId: text('customerId').notNull(),
+  priceId: text('priceId').notNull(),
+  status: text('status').notNull(),
+  currentPeriodEnd: timestamp('currentPeriodEnd', { mode: 'date' }),
+  createdAt: timestamp('createdAt', { mode: 'date' }).notNull(),
+  updatedAt: timestamp('updatedAt', { mode: 'date' }).notNull(),
+});
