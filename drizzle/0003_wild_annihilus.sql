58364c29f44709715e8116bba87fe55ded55a0c9
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-17 00:42:32 +0530

    Delete drizzle directory

--- drizzle/0003_wild_annihilus.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "project" ALTER COLUMN "isTemplate" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "project" ALTER COLUMN "isPro" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "project" ADD COLUMN "updatedAt" timestamp NOT NULL;
